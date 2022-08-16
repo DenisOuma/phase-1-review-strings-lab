4a57fd9c00cca74d0411f03ad6c094f847d31c6f
DenisOuma <dennisouma38@gmail.com>
2022-08-17 00:17:35 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = "Welcome to Flatbook, ";
+const welcomeMessage = currentUser + "!";
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser + "!";
+
+let shortGreeting = "Welcome, " + "w" + "!";
